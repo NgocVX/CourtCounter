764e362763f714f0ef8964aea3ae2ed3193284f7
NgocVX <vxngoc77@gmail.com>
2018-06-01 13:17:25 +0700

    Initial commit

---
+++ app/src/main/java/com/ngoc_vx/courtcounter/MainActivity.java
@@ -0,0 +1,91 @@
+package com.ngoc_vx.courtcounter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // tracks the score for team A
+    int scoreTeamA = 0;
+
+    // tracks the score for team B
+    int scoreTeamB = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        displayForTeamA(0);
+    }
+
+    /**
+     * Displays the given score for team A
+     *
+     * @param score
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given score for team B
+     *
+     * @param score
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void addThreeForTeamA(View view) {
+        scoreTeamA += 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addTwoForTeamA(View view) {
+        scoreTeamA += 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addOneForTeamA(View view) {
+        scoreTeamA += 1;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addThreeForTeamB(View view) {
+        scoreTeamB += 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addTwoForTeamB(View view) {
+        scoreTeamB += 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addOneForTeamB(View view) {
+        scoreTeamB += 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void resetScore(View view) {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+    }
+
+    /*public void addPointForTeamA(View v) {
+        if (v.getId() == R.id.threePointsButton) {
+            displayForTeamA(3);
+        }
+        else if (v.getId() == R.id.twoPointsButton) {
+            displayForTeamA(2);
+        }
+        else if (v.getId() == R.id.freeThrowButton) {
+            displayForTeamA(1);
+        }
+    }*/
+}
